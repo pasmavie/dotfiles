5c61c6e090a04e92d502e864a0de819b0fb338f1
Gio <kayaularge@gmail.com>
2019-11-04 14:59:51 +0100

    .

--- init.sh
+++ init.sh
@@ -9,18 +9,22 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -s ~/dotfiles/shells/bashrc ~/.bash_profile;
   #profile, if doesnt exist
   touch ~/.profile
-  #chunkwm
-  chmod +x chunkwmrc
-  chmod +x skhdrc
-  ln -s ~/dotfiles/chunkwm/chunkwmrc ~/.chunkwmrc
-  ln -s ~/dotfiles/chunkwm_plugins ~/.chunkwm_plugins
-  ln -s ~/dotfiles/chunkwm/skhdrc ~/.skhdrc
-  if [ -d ~/.config/kitty ]; then
-    mv ~/.config/kitty/kitty.conf ~/.config/kitty/oldkitty.conf
-    ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
-  else
-    echo "can't find a .config dir for kitty terminal, make sure the program is installed correctly"
-  fi
+  ###########
+  # chunkwm #
+  ###########
+
+  # chmod +x chunkwmrc
+  # chmod +x skhdrc
+  # ln -s ~/dotfiles/chunkwm/chunkwmrc ~/.chunkwmrc
+  # ln -s ~/dotfiles/chunkwm_plugins ~/.chunkwm_plugins
+  # ln -s ~/dotfiles/chunkwm/skhdrc ~/.skhdrc
+  # if [ -d ~/.config/kitty ]; then
+  #   mv ~/.config/kitty/kitty.conf ~/.config/kitty/oldkitty.conf
+  #   ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
+  # else
+  #   echo "can't find a .config dir for kitty terminal, make sure the program is installed correctly"
+  # fi
+  
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   ln -s ~/dotfiles/shells/zshrc ~/.zshrc
   ln -s ~/dotfiles/shells/bash_profile ~/.bashrc
